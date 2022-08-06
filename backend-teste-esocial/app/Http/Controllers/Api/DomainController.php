452ff64d31cdb69d817910a2fcc051a3edd46602
Luiz Henrique Santos <luiz.santos85@gmail.com>
2022-08-06 11:08:33 -0300

    implementado validacoes de requisicoes

--- backend-teste-esocial/app/Http/Controllers/Api/DomainController.php
+++ backend-teste-esocial/app/Http/Controllers/Api/DomainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateDomainFormRequest;
 use Illuminate\Http\Request;
 use App\Models\Domain;
 
@@ -21,13 +22,13 @@ class DomainController extends Controller
         return response()->json($domain, 200);
     }
 
-    public function store(Request $request)
+    public function store(StoreUpdateDomainFormRequest $request)
     {
         $domain = $this->domain->create($request->all());
         return response()->json($domain, 201);
     }
 
-    public function update(Request $request, $id)
+    public function update(StoreUpdateDomainFormRequest $request, $id)
     {
         $domain = $this->domain->find($id);
         if (!$domain) {
